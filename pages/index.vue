e14dcb20d83ce721f4affff62909db83c98c01d0
pussitaja <work@jaan.biz>
2023-09-27 15:45:21 +0300

    TOP3

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <Head>
     <title>Pizza Takso – Keegi ei jää ilma</title>
-    <meta name="description" content="Kui mul kõht on Pizza Takso, väga tühi Pizza Takso siis ma tellin suure pizza Pizza Taksoga.">
-    <meta property="og:image" content="/og-image.jpg">
+    <meta
+      name="description"
+      content="Kui mul kõht on Pizza Takso, väga tühi Pizza Takso siis ma tellin suure pizza Pizza Taksoga."
+    />
+    <meta property="og:image" content="/og-image.jpg" />
     <link
       rel="apple-touch-icon"
       sizes="180x180"
@@ -32,14 +35,19 @@
   <TopList>
     <span style="text-decoration: underline">Parim pizza selge see:</span>
     <ol>
+      <li>Suvila</li>
       <li>La Prima</li>
-      <li>Kajakas</li>
-      <li>Jahu</li>
+      <li>Peetri</li>
     </ol>
   </TopList>
   <BgVideo />
   <Banner w="200px" h="100px" pos="left" img="/auto.gif" />
   <Banner w="100px" h="300px" pos="right" img="/auto.gif" />
   <Banner w="100vw" h="40px" pos="bottom" img="/auto.gif" />
-  <div><img src='https://www.free-website-hit-counter.com/c.php?d=9&id=134951&s=24' border='0' /></div>
+  <div>
+    <img
+      src="https://www.free-website-hit-counter.com/c.php?d=9&id=134951&s=24"
+      border="0"
+    />
+  </div>
 </template>
