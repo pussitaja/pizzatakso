c19b661075a19e4a3a9cad69f51da5ab9ce54467
pussitaja <work@jaan.biz>
2024-10-31 19:32:08 +0200

    feat: anim speed

--- components/logo.vue
+++ components/logo.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="logo">
-        <img src="/logo.gif" alt="logo" />
-    </div>
+  <div class="logo">
+    <img src="/logo.gif" alt="logo" />
+  </div>
 </template>
 
 <style lang="scss">
-    .logo {
-        width: 300px;
-        animation: pizzaRotate 10s infinite linear;
-        position: absolute;
-        top: 20px;
-        z-index: 100;
-        pointer-events: none;
-        img {
-            width: 100%;
-        }
-    }
+.logo {
+  width: 300px;
+  animation: pizzaRotate 15s infinite linear;
+  position: absolute;
+  top: 20px;
+  z-index: 100;
+  pointer-events: none;
+  img {
+    width: 100%;
+  }
+}
 </style>
